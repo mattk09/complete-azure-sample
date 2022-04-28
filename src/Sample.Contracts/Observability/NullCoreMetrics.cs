2b633625aec884728a4fe0a8355eb810e2f6a8ed
Matt <mattklup>
2022-04-27 17:55:49 -0700

    Add/Fix Null implementations for metrics/telemetry

---
+++ src/Sample.Contracts/Observability/NullCoreMetrics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sample.Observability
+{
+    public class NullCoreMetrics : ICoreMetrics
+    {
+        public void ApplicationInfo()
+        {
+        }
+
+        public void OnException(Exception exception)
+        {
+        }
+    }
+}
